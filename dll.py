c8d199d8628985c22cea41043dc758d86c8627b1
jonbg22 <jonbg22@ru.is>
2023-02-21 21:34:57 +0000

    still dealing with partition

--- dll.py
+++ dll.py
@@ -6,8 +6,8 @@ class Node:
 
 class DLL:
     def __init__(self):
-        self.head = Node()
-        self.tail = Node()
+        self.head = Node("Head")
+        self.tail = Node("Tail")
 
         self.head.next = self.tail
         self.tail.prev = self.head
@@ -17,15 +17,16 @@ class DLL:
 
     def insert(self, data):
         new_node = Node(data)
-        if self.size == 0:
-            self.head.next = new_node
+        if self.head.next == self.tail:
+            self.tail.prev.next = new_node
+            new_node.prev = self.tail.prev
             self.tail.prev = new_node
             new_node.next = self.tail
-            new_node.prev = self.head
             self.current = new_node
+        elif self.current is None:
+            return
         else:
             self.current.prev.next = new_node
-            # self.current.next.prev = new_node
             new_node.next = self.current
             new_node.prev = self.current.prev
             self.current.prev = new_node
@@ -35,7 +36,7 @@ class DLL:
             
 
     def remove(self):
-        if self.current == None:
+        if self.current is None or self.current == self.head or self.current == self.tail:
             return
         elif self.size == 1:
             self.head.next = self.tail
@@ -44,50 +45,41 @@ class DLL:
         else:
             self.current.prev.next = self.current.next
             self.current.next.prev = self.current.prev
-            if self.current.next == self.tail:
-                self.current = None
-            else:
-                self.current = self.current.next
+            self.current = self.current.next
         self.size -= 1
         
 
     def get_value(self):
-        if self.current == None:
+        if self.current is None:
             return
         return self.current.data
 
     def move_to_next(self):
-        if self.current == None:
+        if self.current is None or self.current.next is None:
             return
-        if self.current.next == self.tail:
-            return
-        self.current = self.current.next
+        else:
+            self.current = self.current.next
 
     def move_to_prev(self):
-        if self.current == None:
+        if self.current is None or self.current.prev == self.head:
             return
-        if self.current.prev == self.head:
-            return
-        self.current = self.current.prev
+        else:
+            self.current = self.current.prev
 
     def move_to_pos(self, pos):
-        if 0 > self.size or abs(pos) > self.size-1:
+        if pos < 0 or pos > self.size:
             return
-        
-        if pos >= 0:
-            cur = self.head.next
-            for _ in range(pos):
-                cur = cur.next
-        elif pos < 0:
-            cur = self.tail.prev
-            for _ in range(abs(pos)):
-                cur = cur.prev
+            
+        cur = self.head.next
+        for _ in range(pos):
+            cur = cur.next
+
         self.current = cur 
 
     def clear(self):
-        self.current = None
         self.head.next = self.tail
-        self.tail.prev = self.tail
+        self.tail.prev = self.head
+        self.current = None
         self.size = 0
 
     def get_first_node(self):
@@ -101,36 +93,49 @@ class DLL:
         return self.tail.prev
 
     def partition(self, low, high):
-        # print("Entered Partition with low:",low.data,"high:",high.data)
-        ind = 0
-        self.current = low
+        print("Entered Partition with low:",low.data,"high:",high.data)
+        if low.next == self.tail or low.next == None:
+            return
+        cur = low.next
+        flag = False
         while True:
-            # print("Index:",ind)
-            print(self.current.data)
-            # print("Before Change:",self.current.data, i.data)
-            if self.get_value() <= low.data and self.current != low:
-                val = self.get_value()
-                self.remove()
-                self.current = low
-                self.insert(val)
-            if self.current == high or ind == 50:
-                print("Broke")
+            print("CUR:",str(self))
+            if cur == None:
                 break
-            self.move_to_pos(ind+1)
-            ind += 1
+            if cur.data < low.data:
+                moved = Node(cur.data)
+                self.current = cur
+                self.remove()
+                low.prev.next = moved
+                moved.next = low
+                moved.prev = low.prev
+                low.prev = moved
+            if cur == high:    
+                break
+            cur = cur.next
         self.current = low
 
+    def is_sorted(self):
+        cur = self.head.next
+        while cur.next != self.tail:
+            if cur.data > cur.next.data:
+                return False
+            cur = cur.next
+        return True    
 
     def quick_sort(self,low,high):
-        if low != high:
+        if low != high and low != None and low != high.next:
             self.partition(low,high)
             pi = self.current
             self.quick_sort(low,pi.prev)
             self.quick_sort(pi.next,high)
 
 
+        
+
     def sort(self):
-        self.quick_sort(self.get_first_node(),self.get_last_node())        
+        self.quick_sort(self.get_first_node(),self.get_last_node())    
+        self.current = self.head    
         
         
     def is_sorted(self):
@@ -152,6 +157,13 @@ class DLL:
             cur_node = cur_node.next
         return ret_str
 
+    def print_backwards(self):
+        print_string = ""
+        node = self.tail.prev
+        while node != self.head:
+            print_string += str(node.data) + " "
+            node = node.prev
+        print(print_string)
 
 def randomNumbers(amount,low,high):
     from random import randint
@@ -164,13 +176,17 @@ def randomNumbers(amount,low,high):
 if __name__ == "__main__":
     #create tests here if you want
     dll = DLL()
-    # for num in [6, 3, 1, 14, 17, 1, 10]:
-    for num in randomNumbers(10,1,20):
+    # for num in [6, 3, 1, 14, 17, 1, 10]: # 10 1 17 14 1 3 6 
+    for num in randomNumbers(10,1,30):
         dll.insert(num)
     print(dll)
-    dll.partition(dll.get_first_node(),dll.get_last_node())
+    # dll.partition(dll.get_first_node(),dll.get_first_node().next.next)
+    dll.sort()
     print(dll)
-    print(dll.get_value())
+    
+
+    
+
     # dll.sort()
     # print(dll)
 
